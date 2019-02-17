6261155639b235ec72324fa314a5d92d552bad86
daple1997 <36828075+daple1997@users.noreply.github.com>
2019-02-17 12:33:59 -0700

    Create main.java

---
+++ main.java
@@ -0,0 +1,13 @@
+/**
+ * This is the driver class of the whole program
+ * @author Daniil Pleskach Team: /NewFolder(2)
+ * version 1.0
+ */
+public class appDriver {
+	
+	public static void main(String[] args) {
+		
+		backend back = new backend();
+		
+	}
+}
